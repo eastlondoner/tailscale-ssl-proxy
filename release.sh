145f2d43ac4892ad53106b15359812b03518b143
Andrew Jefferson <andy@mandadata.com>
2022-01-29 20:46:14 +0100

    tweak the release script

--- release.sh
+++ release.sh
@@ -3,7 +3,7 @@
 # make sh play nicely
 set -euo pipefail
 
-git tag -a "$1" -m "Release $1" && git push --tags
+git tag -a "$1" -m "Release $1" && git push --tags || git tag -d "$1"
 docker build --target release -t tailscale-ssl-proxy_build-release .
 docker compose --env-file .auth/github.env -f docker-compose.build.yml up
 docker compose -f docker-compose.build.yml down
